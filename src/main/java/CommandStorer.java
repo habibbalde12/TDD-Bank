ef68323f1405cec92104801618b3eb64b8e3eda3
Habib Balde <hab98@drexel.edu>
2025-08-09 02:28:13 -0400

    Finished CommandStorer

---
+++ src/main/java/CommandStorer.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandStorer {
+    private final List<String> invalid = new ArrayList<>();
+
+    public void storeInvalid(String command) {
+        invalid.add(command);
+    }
+
+    public List<String> getInvalid() {
+        return new ArrayList<>(invalid); // return a copy
+    }
+
+    public void clear() {
+        invalid.clear();
+    }
+}
+
