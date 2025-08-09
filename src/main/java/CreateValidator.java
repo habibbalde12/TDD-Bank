8a27f5b98b7f8c98901350710209108040f34722
Habib Balde <hab98@drexel.edu>
2025-08-09 02:03:29 -0400

    Made slight changes to the validators

--- src/main/java/CreateValidator.java
+++ src/main/java/CreateValidator.java
@@ -6,19 +6,27 @@ public class CreateValidator extends CommandValidator {
 
 
     public boolean validate(String[] tokens) {
-        if (tokens == null || tokens.length != 4) return false;
+        if (tokens == null || tokens.length != 4)
+            return false;
+        if (!"create".equals(tokens[0]))
+            return false;
 
         String type = tokens[1];
         String accountId = tokens[2];
         String rate = tokens[3];
 
-        if (!type.equals("savings") && !type.equals("checking")) return false;
-        if (!isEightDigitNumber(accountId)) return false;
-        if (bank.getAccount(accountId) != null) return false;
-        if (!validInterestRate(rate)) return false;
+        if (!type.equals("savings") && !type.equals("checking"))
+            return false;
+        if (!isEightDigitNumber(accountId))
+            return false;
+        if (bank.getAccount(accountId) != null)
+            return false;
+        if (!validInterestRate(rate))
+            return false;
 
         return true;
     }
+
 }
 
 
--- src/main/java/DepositValidator.java
+++ src/main/java/DepositValidator.java
@@ -6,15 +6,22 @@ public class DepositValidator extends CommandValidator {
 
     // Used by DepositValidatorTest
     public boolean validate(String[] tokens) {
-        if (tokens == null || tokens.length != 3) return false;
+        if (tokens == null || tokens.length != 3)
+            return false;
+        if (!"deposit".equals(tokens[0]))
+            return false;
 
         String accountId = tokens[1];
         String amount = tokens[2];
 
-        if (!isEightDigitNumber(accountId)) return false;
-        if (bank.getAccount(accountId) == null) return false;
-        if (!isValidAmount(amount)) return false;
+        if (!isEightDigitNumber(accountId))
+            return false;
+        if (bank.getAccount(accountId) == null)
+            return false;
+        if (!isValidAmount(amount))
+            return false;
 
         return true;
     }
+
 }
