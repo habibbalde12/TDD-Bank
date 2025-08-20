2953185640b29900761ad07086e2b70a0e50d245
Habib Balde <hab98@drexel.edu>
2025-08-20 00:22:46 -0400

    Finished the WithdrawCommandProcess and WithdrawCommandProcessTest

---
+++ src/main/java/banking/WithdrawCommandProcess.java
@@ -0,0 +1,24 @@
+package banking;
+
+public class WithdrawCommandProcess extends CommandProcess {
+    public WithdrawCommandProcess(Bank bank) {
+        super(bank);
+    }
+
+    @Override
+    public boolean supports(String commandType) {
+        if ("withdraw".equalsIgnoreCase(commandType)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void processTokens(String[] tokens) {
+        String id = tokens[1];
+        String amountRaw = tokens[2];
+        double amount = Double.parseDouble(amountRaw);
+        bank.withdraw(id, amount);
+    }
+}
+
---
+++ src/test/java/banking/WithdrawCommandProcessTest.java
@@ -0,0 +1,67 @@
+// WithdrawCommandProcessTest.java
+package banking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WithdrawCommandProcessTest {
+    private Bank bankInstance;
+    private WithdrawCommandProcess withdrawCommandProcessInstance;
+
+    @BeforeEach
+    void setUp() {
+        bankInstance = new Bank();
+        withdrawCommandProcessInstance = new WithdrawCommandProcess(bankInstance);
+    }
+
+    @Test
+    void supports_withdraw_only() {
+        assertTrue(withdrawCommandProcessInstance.supports("withdraw"));
+        assertFalse(withdrawCommandProcessInstance.supports("deposit"));
+        assertFalse(withdrawCommandProcessInstance.supports("create"));
+        assertFalse(withdrawCommandProcessInstance.supports("transfer"));
+        assertFalse(withdrawCommandProcessInstance.supports("pass"));
+    }
+
+    @Test
+    void process_tokens_withdraws_amount_from_checkings() {
+        bankInstance.addAccount(new Checkings("12345678", 1.5));
+        bankInstance.deposit("12345678", 500.0);
+        String[] tokens = {"withdraw", "12345678", "200.0"};
+        withdrawCommandProcessInstance.processTokens(tokens);
+        Account updatedAccount = bankInstance.getAccount("12345678");
+        assertEquals(300.0, updatedAccount.getBalance(), 0.0001);
+    }
+
+    @Test
+    void process_tokens_withdraws_amount_from_savings() {
+        bankInstance.addAccount(new Savings("12345678", 1.0));
+        bankInstance.deposit("12345678", 400.0);
+        String[] tokens = {"withdraw", "12345678", "150.0"};
+        withdrawCommandProcessInstance.processTokens(tokens);
+        Account updatedAccount = bankInstance.getAccount("12345678");
+        assertEquals(250.0, updatedAccount.getBalance(), 0.0001);
+    }
+
+    @Test
+    void process_tokens_overdraft_sets_to_zero_when_amount_exceeds_balance() {
+        bankInstance.addAccount(new Checkings("12345678", 1.0));
+        bankInstance.deposit("12345678", 150.0);
+        String[] tokens = {"withdraw", "12345678", "999.0"};
+        withdrawCommandProcessInstance.processTokens(tokens);
+        Account updatedAccount = bankInstance.getAccount("12345678");
+        assertEquals(0.0, updatedAccount.getBalance(), 0.0001);
+    }
+
+    @Test
+    void process_tokens_zero_amount_no_change() {
+        bankInstance.addAccount(new Savings("12345678", 1.0));
+        bankInstance.deposit("12345678", 250.0);
+        String[] tokens = {"withdraw", "12345678", "0.0"};
+        withdrawCommandProcessInstance.processTokens(tokens);
+        Account updatedAccount = bankInstance.getAccount("12345678");
+        assertEquals(250.0, updatedAccount.getBalance(), 0.0001);
+    }
+}
+
